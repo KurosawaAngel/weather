77a5627172bad3b734106bcc151530a7a82143e2
Kurosawa <145038102+KurosawaAngel@users.noreply.github.com>
2024-10-13 13:24:45 +0500

    unused dumper

--- weather/api/client.py
+++ weather/api/client.py
@@ -1,12 +1,10 @@
-from typing import Sequence
-
-from adaptix import Retort, dumper, name_mapping
+from adaptix import Retort, name_mapping
 from dataclass_rest import get
 from dataclass_rest.client_protocol import FactoryProtocol
 from dataclass_rest.http.aiohttp import AiohttpClient, AiohttpMethod
 from dataclass_rest.http_request import HttpRequest
 
-from weather.api.enums import Exclude, Units
+from weather.api.enums import Units
 from weather.api.models import Coordinates, Weather
 
 BASE_URL = "http://api.openweathermap.org"
@@ -29,9 +27,6 @@ class OpenWeatherClient(AiohttpClient):
     def __init__(self, api_key: str) -> None:
         self._api_key = api_key
         super().__init__(base_url=BASE_URL)
-
-    def _init_request_args_factory(self) -> FactoryProtocol:
-        return Retort(recipe=[dumper(Sequence[Exclude], lambda x: ",".join(x))])
 
     def _init_response_body_factory(self) -> FactoryProtocol:
         return Retort(
